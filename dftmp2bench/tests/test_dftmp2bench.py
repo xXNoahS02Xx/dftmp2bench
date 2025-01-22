2345dd17667b9d005031b000b6933bcc183852c7
EricBoittier <eric.boittier@icloud.com>
2025-01-22 11:41:10 +0100

    ...

--- dftmp2bench/tests/test_dftmp2bench.py
+++ dftmp2bench/tests/test_dftmp2bench.py
@@ -1,9 +1,14 @@
 """
 Unit and regression test for the dftmp2bench package.
+
+To run the tests, run `pytest` from the root directory of the repository.
 """
 
 # Import package, test suite, and other packages as needed
 import sys
+from pathlib import Path
+
+softwares = ["orca", "gaussian", "psi4", "xtb"]
 
 import pytest
 
@@ -13,3 +18,40 @@ import dftmp2bench
 def test_dftmp2bench_imported():
     """Sample test, will always pass so long as import statement worked."""
     assert "dftmp2bench" in sys.modules
+
+
+def test_ccinput():
+    import ccinput
+
+    # current file directory
+    current_dir = Path(__file__).parent
+    xyzfilename = current_dir / ".." / "data" / "xyzs" / "water.xyz"
+
+    from ccinput.wrapper import gen_input
+
+    for software in softwares:
+        if software == "xtb":
+            inp = gen_input(
+                software=software,
+                type="sp",
+                method="GFN2-xTB",
+                file=xyzfilename,
+                nproc=16,
+            )
+        else:
+            inp = gen_input(
+                software=software,
+                type="sp",
+                method="PBE0",
+                basis_set="def2-SVP",
+                file=xyzfilename,
+                nproc=16,
+            )
+        print("SOFTWARE: ", software)
+        print("*" * 80)
+        print(inp)
+        print("*" * 80)
+
+
+if __name__ == "__main__":
+    test_ccinput()
